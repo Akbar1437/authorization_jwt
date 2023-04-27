363a1d7378f4aadf086ac6781eb64c52f42552cd
Akbar Khushnazarov <khusnazarovakbar@gmail.com>
2023-04-27 06:39:37 +0500

    setting react with vite

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>TS Oversize</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  );
+}
+
+export default App;
